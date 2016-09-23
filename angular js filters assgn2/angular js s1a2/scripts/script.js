3c362d60f3353e00d7f6176385aa286dfda1b368
praveenearle <praveen.earle@gmail.com>
2016-09-24 00:35:39 +0530

    angular js filters assgn2
    angular js filters assgn2

---
+++ angular js filters assgn2/angular js s1a2/scripts/script.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.integer = "4.557693";
+
+         
+    }); 
+
